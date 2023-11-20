1ff3a1b83d89af198c604e77166d78cbc7b72f7b
Ese Curtis <curtisese52@gmail.com>
2023-11-20 20:53:56 +0100

    feat:  completed the screen item

--- src/screens/components/ScreenItem.tsx
+++ src/screens/components/ScreenItem.tsx
@@ -1,16 +1,32 @@
-import React, { ReactNode } from "react";
+import React, { ReactNode, useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 
 interface IScreenItem {
+    hash: string;
     children: ReactNode;
 }
 
 const ScreenItem: React.FC<IScreenItem> = ({ hash, children }) => {
-    const hashValue; ///put hash value
+    const location = useLocation();
+    const [hashValue, setHashValue] = useState<string>(location.hash.substring(1));
+
+    useEffect(() => {
+        const handleHashChange = () => {
+            setHashValue(location.hash.substring(1));
+        };
+
+        window.addEventListener("hashchange", handleHashChange);
+
+        return () => {
+            window.removeEventListener("hashchange", handleHashChange);
+        };
+    }, [location]);
+
     return (
-        <div className={`app--screen ${hashValue == hash}`}>
+        <div className={`app--screen ${hashValue === hash ? "active" : "in-active"}`}>
             {children}
         </div>
     );
 };
 
-export default Screens;
+export default ScreenItem;
