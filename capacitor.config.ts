954f352bc205ef406a1cc26f491ac4c73cc41da1
Ese Curtis <curtisese52@gmail.com>
2023-12-03 21:52:35 +0100

    feat: hjust retrying

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ventt.dezrektech',
   appName: 'ventt-io',
-  webDir: 'build',
+  webDir: 'dist',
   server: {
     androidScheme: 'https'
   }
